b5da6bfd70d544945b15df4590fd9dc20209e022
mfaraon1 <97813146+mfaraon1@users.noreply.github.com>
2022-02-10 11:30:25 -0700

    Add files via upload
    I also have the ajax code we can use to connect the javascript to this Python server

---
+++ server.py
@@ -0,0 +1,30 @@
+import flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    print("Page Rendered")
+    return render_template('index.html')
+
+
+
+@app.route('/testConnection', methods=['GET','POST'])
+def testConnection():
+    
+    # POST REQUEST
+    if request.method == 'POST':
+        print("received TEST Post Request")
+        data=request.json['data']
+        print("POST sent: ", data)
+        return json.dumps({'status':'OK'})
+
+    # GET REQUEST
+    if request.method == 'GET':
+        print('received TEST GET request')
+        return 'tester'
+
+if __name__ == "__main__":
+    app.run()
+    print("Server now listening...")
