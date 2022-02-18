26edefe3f843e7a1dceb0c522184d77eeec9ea58
matt <maattheew@yahoo.com>
2022-02-18 08:19:24 -0800

    Admin Overview/Logon Code/Submission Pages

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const coworkers = ['coworkers placeholder 1', 'coworkers placeholder 2', 'cowork
 const unknown = ['unknown placeholder 1', 'unknown placeholder 2', 'unknown placeholder 3', 'unknown placeholder 4', 
                  'unknown placeholder 5'];
 
-let surveyAnswers = [["Greeting", ""], ["Work Life Balance", ""], ["How's Work", ""], ["How's Your CoWorkers", ""], ["Unknown", ""]];
+let surveyAnswers; 
 
 let greetingQuestion;
 let workLifeQuestion;
@@ -47,7 +47,7 @@ function submitAppear() {
         }
 }
 
-function submit() {
+function loginSubmit() {
     let one = document.getElementById('codeOne').value;
     let two = document.getElementById('codeTwo').value;
     let three = document.getElementById('codeThree').value;
@@ -55,38 +55,84 @@ function submit() {
     let five = document.getElementById('codeFive').value;
     let six = document.getElementById('codeSix').value;
 
+    const adminCode = "FD98WD"
+    const empCode = "A2JND8"
+
     let loginCode = one + two + three + four + five + six;
-
-    let loggedOn = 'success';
-
-    if (loggedOn = 'success') {
-        window.location.href = "survey.html"
+    
+    if (loginCode.toUpperCase() == adminCode) {
+        window.location.href = "overview.html";
+    } else if (loginCode.toUpperCase() == empCode){
+        window.location.href = "survey.html";
+    } else {
+        document.getElementById('loginGreeting').innerHTML = "Invalid Code. Try Again!";
+        document.getElementById('loginGreeting').style.color = "red";
+        document.getElementById("codeOne").style.borderColor = "red";
+        document.getElementById("codeTwo").style.borderColor = "red";
+        document.getElementById("codeThree").style.borderColor = "red";
+        document.getElementById("codeFour").style.borderColor = "red";
+        document.getElementById("codeFive").style.borderColor = "red";
+        document.getElementById("codeSix").style.borderColor = "red";
     }
 }
 
 function submitAnswers() {
-    
+    surveyAnswers = [["Greeting", greetingAnswer], ["Work Life Balance", workLifeAnswer], ["How's Work", howsWorkAnswer], 
+                     ["How's Your CoWorkers", coworkersAnswer], ["Unknown", unknownAnswer]];
+    window.location.href = "submission.html"
+    console.log(surveyAnswers);
 }
 
-function nextQuestion() {
+function nextQuestion(answerValue) {
     if (currentQuestion == greetingQuestion) {
+        greetingAnswer = answerValue;
         currentQuestion = workLifeQuestion;
         document.getElementById('questionID').innerHTML = currentQuestion;
     } else if (currentQuestion == workLifeQuestion) {
+        workLifeAnswer = answerValue;
         currentQuestion = howsWorkQuestion;
         document.getElementById('questionID').innerHTML = currentQuestion;
     } else if (currentQuestion == howsWorkQuestion) {
+        howsWorkAnswer = answerValue;
         currentQuestion = coworkersQuestion;
         document.getElementById('questionID').innerHTML = currentQuestion;
     } else if (currentQuestion == coworkersQuestion) {
+        coworkersAnswer = answerValue;
         currentQuestion = unknownQuestion;
         document.getElementById('questionID').innerHTML = currentQuestion;
     } else {
+        unknownAnswer = answerValue;
         document.getElementById('submitAnswers').style.visibility = "visible";
         document.getElementById('submitAnswers').style.transitionDuration = "1s";
         document.getElementById('submitAnswers').style.opacity = "1";
     }
 
+}
+
+function empCodeGenerator() {
+    let result = "";
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    for (i=0; i<6; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result;
+}
+
+function outputEmpCode() {
+    let validEmpId = "JB23MD";
+    let newEmpCode = "";
+    let employeeID = document.getElementById('empIdInput').value;
+
+    if (employeeID == validEmpId) {
+        newEmpCode = empCodeGenerator();
+        document.getElementById('codeOutput').innerHTML = newEmpCode;
+        document.getElementById('codeOutput').style.fontSize = "20px";
+        document.getElementById('codeOutput').style.color = "black";
+    } else {
+        document.getElementById('codeOutput').innerHTML = "Employee ID Is Already In Use Or Invalid";
+        document.getElementById('codeOutput').style.fontSize = "10px";
+        document.getElementById('codeOutput').style.color = "red";
+    }
 }
 
 function surveyQuestions() {
@@ -98,6 +144,124 @@ function surveyQuestions() {
 
     document.getElementById('questionID').innerHTML = greetingQuestion;
     currentQuestion = greetingQuestion;
+}
+
+function createOverview() {
+    let greetingChart = document.getElementById('greetingOverview').getContext('2d');
+    let workLifeChart = document.getElementById('workLifeOverview').getContext('2d');
+    let howsWorkChart = document.getElementById('howsWorkOverview').getContext('2d');
+    let coworkersChart = document.getElementById('coworkersOverview').getContext('2d');
+    let unknownChart = document.getElementById('unknownOverview').getContext('2d');
+
+    let greetDoughChart = new Chart(greetingChart, {
+        type: 'doughnut',
+        data: {
+            labels: ['Mad', 'Sad', 'Content', 'Joyful', 'Happy'],
+            datasets: [{
+                label: 'Amount',
+                data: [43, 34, 243, 654, 432],
+                backgroundColor: [
+                    '#754fa0', '#F2D40D', '#90D36B', '#09B7BF', '#ED1B72'
+                ]
+            }]
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
+
+    let workLifeDoughChart = new Chart(workLifeChart, {
+        type: 'doughnut',
+        data: {
+            labels: ['Mad', 'Sad', 'Content', 'Joyful', 'Happy'],
+            datasets: [{
+                label: 'Amount',
+                data: [26, 45, 382, 462, 217],
+                backgroundColor: [
+                    '#754fa0', '#F2D40D', '#90D36B', '#09B7BF', '#ED1B72'
+                ]
+            }]
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
+
+    let howsWorkDoughChart = new Chart(howsWorkChart, {
+        type: 'doughnut',
+        data: {
+            labels: ['Mad', 'Sad', 'Content', 'Joyful', 'Happy'],
+            datasets: [{
+                label: 'Amount',
+                data: [101, 31, 243, 354, 132],
+                backgroundColor: [
+                    '#754fa0', '#F2D40D', '#90D36B', '#09B7BF', '#ED1B72'
+                ]
+            }]
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
+
+    let coworkersDoughChart = new Chart(coworkersChart, {
+        type: 'doughnut',
+        data: {
+            labels: ['Mad', 'Sad', 'Content', 'Joyful', 'Happy'],
+            datasets: [{
+                label: 'Amount',
+                data: [43, 18, 643, 224, 82],
+                backgroundColor: [
+                    '#754fa0', '#F2D40D', '#90D36B', '#09B7BF', '#ED1B72'
+                ]
+            }]
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
+
+    let unknownDoughChart = new Chart(unknownChart, {
+        type: 'doughnut',
+        data: {
+            labels: ['Mad', 'Sad', 'Content', 'Joyful', 'Happy'],
+            datasets: [{
+                label: 'Amount',
+                data: [73, 102, 311, 438, 132],
+                backgroundColor: [
+                    '#754fa0', '#F2D40D', '#90D36B', '#09B7BF', '#ED1B72'
+                ]
+            }]
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    display: false
+                }
+            }
+        }
+    });
 }
 
 function TestButtonHandler() {
@@ -123,3 +287,4 @@ function TestButtonHandler() {
         }
     });
 }
+
