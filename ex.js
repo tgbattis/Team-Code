18b96791f75713de6df4c3fb34266806c0137a78
unknown <hibbingm@gmail.com>
2022-02-27 23:09:55 -0700

    testing

--- ex.js
+++ ex.js
@@ -1,8 +1,34 @@
-questionOneAnswers = [['mad', 2], ['sad,', 3], ['content', 8], ['joyful', 7], ['happy', 3]];
+questionOneAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionTwoAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionThreeAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionFourAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionFiveAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionSixAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionSevenAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionEightAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionNineAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionTenAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionElevenAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionTwelveAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionThirteenAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionFourteenAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
+questionFifteenAnswers = [['mad', 0,] ['sad,', 0], ['content', 0], ['joyful', 0], ['happy', 0]];
 
 //surveyAnswers[0] = [firstSurveyQuestion, firstAnswer]
 outputAnswersOne = surveyAnswers[0]
 
-if (surveyAnswers[0][1] == -2) {
-    questionOneAnswers[0][1] += 1;
-}
+    if (surveyAnswers[0][1] == -2) {
+        questionOneAnswers[0][1] += 1;
+    }
+    if (surveyAnswers[0][1] == -2) {
+        questionOneAnswers[0][1] += 1;
+    }
+    if (surveyAnswers[0][1] == -2) {
+        questionOneAnswers[0][1] += 1;
+    }
+    if (surveyAnswers[0][1] == -2) {
+        questionOneAnswers[0][1] += 1;
+    }
+    if (surveyAnswers[0][1] == -2) {
+        questionOneAnswers[0][1] += 1;
+    }
